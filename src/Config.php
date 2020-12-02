2012efb19ae8c3464ce3ac34180422e3e3352732
benjamin <benjamin.leyrat.24@gmail.com>
2020-12-02 15:53:11 +0100

    Drop db/table

--- src/Config.php
+++ src/Config.php
@@ -13,11 +13,15 @@ class Config{
     const DB_TABLE = "Comment";
     const PORT = "3308";
 
+    public static function dbConf(){
+        $db = new PDO("mysql:host=".self::SERV_NAME.";port=".self::PORT, self::USERNAME, self::PASSWORD);
+        $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        return $db;
+    }
+
     public static function createDB(){
         try{
-            $nameDB = self::DB_NAME;
-            $db = new PDO("mysql:host=".self::SERV_NAME.";port=".self::PORT, self::USERNAME, self::PASSWORD);
-            $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            $db = self::dbConf();
             $sql = "CREATE DATABASE ".self::DB_NAME;
             $db->exec($sql);
             echo 'Base de données '.self::DB_NAME.' créée !';
@@ -30,17 +34,41 @@ class Config{
         try{
             $dbName = self::DB_NAME;
             $dbTable = self::DB_TABLE;
-            $db = new PDO("mysql:host=".self::SERV_NAME.";port=".self::PORT, self::USERNAME, self::PASSWORD);
-            $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            $db = self::dbConf();
             $sql = "CREATE TABLE {$dbName}.{$dbTable} (
-                PersonID int,
-                LastName varchar(255),
                 FirstName varchar(255),
-                Address varchar(255),
-                City varchar(255)
+                LastName varchar(255),
+                Comment text,
+                created_at DATETIME
             )";
             $db->exec($sql);
             echo 'Table '.self::DB_TABLE.' créée !';
+        }catch(PDOException $e){
+          echo "Erreur : " . $e->getMessage();
+        }
+    }
+
+    public static function dropDB(){
+        try{
+            $dbName = self::DB_NAME;
+            $dbTable = self::DB_TABLE;
+            $db = self::dbConf();
+            $sql = "DROP DATABASE {$dbName}";
+            $db->exec($sql);
+            echo 'Base de données '.self::DB_TABLE.' supprimé !';
+        }catch(PDOException $e){
+          echo "Erreur : " . $e->getMessage();
+        }
+    }
+
+    public static function dropTable(){
+        try{
+            $dbName = self::DB_NAME;
+            $dbTable = self::DB_TABLE;
+            $db = self::dbConf();
+            $sql = "DROP TABLE {$dbName}.{$dbTable}";
+            $db->exec($sql);
+            echo 'Table '.self::DB_TABLE.' supprimé !';
         }catch(PDOException $e){
           echo "Erreur : " . $e->getMessage();
         }
