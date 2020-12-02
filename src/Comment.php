d55cfa3294b726fa03ea54a486c2e0a2f1b151b1
benjamin <benjamin.leyrat.24@gmail.com>
2020-12-02 13:22:22 +0100

    Create db/table

--- src/Comment.php
+++ src/Comment.php
@@ -3,7 +3,9 @@ namespace App;
 
 use App\Config;
 
+// http://127.0.0.1:8000/src/Comment.php
 define('DS', DIRECTORY_SEPARATOR);
 require dirname(__DIR__) .DS.'vendor'.DS.'autoload.php';
 
-Config::create();
+// Config::createDB();
+// Config::createTable();
--- src/Config.php
+++ src/Config.php
@@ -9,16 +9,38 @@ class Config{
     const SERV_NAME = "localhost";
     const USERNAME = "root";
     const PASSWORD = "";
-    const DB_NAME = "Comment";
+    const DB_NAME = "Plugin";
+    const DB_TABLE = "Comment";
     const PORT = "3308";
 
-    public static function create(){
+    public static function createDB(){
         try{
+            $nameDB = self::DB_NAME;
             $db = new PDO("mysql:host=".self::SERV_NAME.";port=".self::PORT, self::USERNAME, self::PASSWORD);
             $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             $sql = "CREATE DATABASE ".self::DB_NAME;
             $db->exec($sql);
-            echo 'Base de données'.self::DB_NAME.'créée!';
+            echo 'Base de données '.self::DB_NAME.' créée !';
+        }catch(PDOException $e){
+          echo "Erreur : " . $e->getMessage();
+        }
+    }
+
+    public static function createTable(){
+        try{
+            $dbName = self::DB_NAME;
+            $dbTable = self::DB_TABLE;
+            $db = new PDO("mysql:host=".self::SERV_NAME.";port=".self::PORT, self::USERNAME, self::PASSWORD);
+            $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            $sql = "CREATE TABLE {$dbName}.{$dbTable} (
+                PersonID int,
+                LastName varchar(255),
+                FirstName varchar(255),
+                Address varchar(255),
+                City varchar(255)
+            )";
+            $db->exec($sql);
+            echo 'Table '.self::DB_TABLE.' créée !';
         }catch(PDOException $e){
           echo "Erreur : " . $e->getMessage();
         }
